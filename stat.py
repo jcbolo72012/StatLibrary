2d994bf73f9938d5041f3d1cc6269657def00e7a
John Bolognino <jcbolo72012@gmail.com>
2021-01-07 15:32:33 -0500

    Added quickselect method for median

--- stat.py
+++ stat.py
@@ -1,5 +1,8 @@
 import math
+import random
 
+#### Section 1: Measures of Variance and Central Tendancy
+## mean, median, mode, standard variance, standard deviation, and confidence interval
 def mean(data):
     """The mean or average of a set of data is the sum of all data
         points divided by the amount of data points.
@@ -32,3 +35,37 @@ def confidence_interval(data):
         return
     standard_deviation(data)/math.sqrt(len(data))
 
+def median(data):
+    """ Finds the median using Tony Hoare's recursive quickselect algorithm
+        This method picks the appropriate way to call the recursive method based on
+        whether the dataset has an even or odd number of members
+
+        Average of O(n) time
+    """
+    pivot_fn = random.choice
+    if len(data) % 2 == 1:  # case for datasets of even length
+        return quickselect(data, len(data) // 2, pivot_fn) 
+    else: # case for odd length
+        0.5 * (quickselect(data, len(data) / 2 - 1, pivot_fn) + quickselect(data, len(data) / 2, pivot_fn))
+
+
+
+def quickselect(data, index, pivot_fn):
+    if len(data) == 1:
+        assert index == 0
+        return data[0]
+
+    pivot = pivot_fn(data)
+
+    less = [x for x in data if x<pivot] # all points less than the chosen pivot
+    greater = [x for x in data if x>pivot] # all points greater than the chosen pivot
+    equal = [x for x in data if x == pivot] # all points equal to the chosen pivot
+
+    if index < len(less): # if 
+        return quickselect(less, index, pivot_fn)
+    elif index < len(less) + len(equal):
+        return equal[0]
+    else:
+        return quickselect(greater, index - (len(less) + len(equal)), pivot_fn)
+
+print(median([5,6,3,2,4,7,1]))
